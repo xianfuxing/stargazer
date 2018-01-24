f1b67858c7c666c2c48e9cc8dbf4caf25febb7f2
xianfuxing <xianfuxing@126.com>
2018-01-24 11:37:07 +0800

    # add zapi cache

--- apps/monitor/mixins/zapi.py
+++ apps/monitor/mixins/zapi.py
@@ -1,16 +1,23 @@
 from django.conf import settings
+from django.core.cache import cache
 from pyzabbix import ZabbixAPI
-
 
 class ZapiMixin(object):
     """
      Mixin for return zapi
     """
     def get_zapi(self):
+        # Get zapi cache if exist
+        zapi = cache.get('zapi')
+        if zapi:
+            return zapi
+
         ZAPI_URL = getattr(settings, 'ZAPI_URL', '')
         ZAPI_USER = getattr(settings, 'ZAPI_USER', '')
         ZAPI_PASSWORD = getattr(settings, 'ZAPI_PASSWORD', '')
         zapi = ZabbixAPI(ZAPI_URL)
         zapi.login(ZAPI_USER, ZAPI_PASSWORD)
 
+        # Cache zapi if not hit
+        cache.set('zapi', zapi, 600)
         return zapi
