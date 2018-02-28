343ae1564464d7d6ccd2243cccc85ae975bec9e2
xianfuxing <xianfuxing@126.com>
2018-02-28 15:52:02 +0800

    # bug fixed

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import render
+from django.http import JsonResponse
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.signals import user_login_failed
 from django.conf import settings
@@ -23,25 +24,31 @@ class MYLoginView(LoginView):
         Handle POST requests: instantiate a form instance with the passed
         POST variables and then check if it's valid.
         """
+        # If failures is gt 3, than use captcha form instead.
         form = self.get_form()
+        failures = self.get_failures(request)
+        if failures >= self.login_failures:
+            form = CaptchaLoginForm(**self.get_form_kwargs())
         if form.is_valid():
             return self.form_valid(form)
         else:
-            csrftoken = request.COOKIES.get('csrftoken', '')
+            csrf_token = request.COOKIES.get('csrftoken', '')
             user_login_failed.send(
                 sender=__name__,
                 request=request,
                 credentials={
-                    'csrftoken': csrftoken
+                    'csrftoken': csrf_token
                 }
             )
             # Check if failed login more than three times
-            failures = cache.get(csrftoken, 0)
+            failures = self.get_failures(request)
             if failures >= self.login_failures:
                 form = CaptchaLoginForm(**self.get_form_kwargs())
+                print(form.errors)
             return self.form_invalid(form)
 
     def get_failures(self, request):
         csrftoken = request.COOKIES.get('csrftoken', '')
         failures = cache.get(csrftoken, 0)
         return failures
+
