30c1882b7d8a9e3856a17c87279126d470c56a36
xianfuxing <xianfuxing@126.com>
2018-03-13 14:13:30 +0800

    # update user model

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+USER_FIELDS = (
+    (None, {'fields': ('username', 'email', 'password1', 'password2',)}),
+)
+
+
+class UserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'position', 'is_staff')
+    add_fieldsets = USER_FIELDS
+
+
+admin.site.register(User, UserAdmin)
--- apps/users/models.py
+++ apps/users/models.py
@@ -3,11 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    department = models.CharField(max_length=20, default='', verbose_name='部门')
-    position = models.CharField(max_length=50, default='', verbose_name='职位')
+    department = models.CharField(max_length=20, null=True, blank=True, default='', verbose_name='部门')
+    position = models.CharField(max_length=50, null=True, blank=True, default='', verbose_name='职位')
 
     class Meta:
-        verbose_name = '用户信息'
+        verbose_name = '用户'
         verbose_name_plural = verbose_name
 
     def __str__(self):
