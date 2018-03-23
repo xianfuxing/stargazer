8a2719d776ff414750d8eabdde80d56fb5f4b717
xianfuxing <xianfuxing@126.com>
2018-03-23 17:39:25 +0800

    # add ssl list page

--- apps/asset/urls.py
+++ apps/asset/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import AssetOverviewView
+from .views import AssetOverviewView, SslListView
 
 
 app_name = 'asset'
 
 urlpatterns = [
-    path('', AssetOverviewView.as_view(), name='overview')
+    path('', AssetOverviewView.as_view(), name='overview'),
+    path('ssl/', SslListView.as_view(), name='ssl')
 ]
--- apps/asset/views.py
+++ apps/asset/views.py
@@ -1,5 +1,12 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from .models import SslCertificate
 
 
 class AssetOverviewView(TemplateView):
     template_name = 'asset/overview.html'
+
+
+class SslListView(ListView):
+    model = SslCertificate
+    context_object_name = 'ssl_list'
+    template_name = 'asset/ssl_list.html'
