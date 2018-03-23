6b6930a4734b727b6f784a62b5687f38248e7548
xianfuxing <xianfuxing@126.com>
2018-03-23 17:44:19 +0800

    # update

--- apps/asset/views.py
+++ apps/asset/views.py
@@ -10,3 +10,7 @@ class SslListView(ListView):
     model = SslCertificate
     context_object_name = 'ssl_list'
     template_name = 'asset/ssl_list.html'
+
+    def get_queryset(self):
+        query_set = super().get_queryset()
+        return query_set.order_by('domain')
