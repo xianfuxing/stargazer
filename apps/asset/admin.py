9d5b3c01b925ab02dff92ae05f1b6f12850b4773
xianfuxing <xianfuxing@126.com>
2018-03-23 15:01:28 +0800

    # initial ssl model

--- apps/asset/admin.py
+++ apps/asset/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import SslCertificate
 
-# Register your models here.
+
+class SslAdmin(admin.ModelAdmin):
+    list_display = ('domain', 'issuer', 'cert_type', 'validity', 'expiry_date')
+    list_filter = ('domain', 'issuer', 'cert_type')
+    ordering = ['domain']
+
+
+admin.site.register(SslCertificate, SslAdmin)
--- apps/asset/apps.py
+++ apps/asset/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig
 
 class AssetConfig(AppConfig):
     name = 'asset'
+    verbose_name = '资产管理'
