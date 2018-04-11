c4221608aff2d6f60936bf77382ef89742d57349
xianfuxing <xianfuxing@126.com>
2018-04-11 18:07:42 +0800

    # initial mugshot upload

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,11 +1,20 @@
+import os
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+
+from imagekit.models import ImageSpecField
+from imagekit.processors import ResizeToFill
+
+
+def mugshot_path(instance, filename):
+    return os.path.join('mugshots', instance.username, filename)
 
 
 class User(AbstractUser):
     phone = models.CharField(max_length=11, null=True, blank=True, default='', verbose_name='电话')
     department = models.CharField(max_length=20, null=True, blank=True, default='', verbose_name='部门')
     position = models.CharField(max_length=50, null=True, blank=True, default='', verbose_name='职位')
+    mugshot = models.ImageField(upload_to=mugshot_path, null=True, blank=True)
 
     class Meta:
         verbose_name = '用户'
