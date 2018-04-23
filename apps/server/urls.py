2ae8bb93e52893c9ff7b9ab3c467ff1f0ba5a429
xianfuxing <xianfuxing@126.com>
2018-04-23 11:35:02 +0800

    # 加上'/'

--- apps/server/urls.py
+++ apps/server/urls.py
@@ -5,6 +5,6 @@ app_name = 'server'
 
 urlpatterns = [
     path('', ServerListView.as_view(), name='list'),
-    path('overview', ServerOverviewView.as_view(), name='overview'),
+    path('overview/', ServerOverviewView.as_view(), name='overview'),
     path('detail/<int:host_id>/', ServerDetailView.as_view(), name='detail'),
 ]
